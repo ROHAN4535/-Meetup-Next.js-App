3b060e3029dd68299b009ba1e1e063d8a14a001f
ROHAN4535 <grohan468@gmail.com>
2023-12-22 15:34:59 +0530

    Worked On Dummy Data Setup

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
+const MeetupDetailsPage = () => {
+  return (
+    <MeetupDetails
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg"
+      title="A first meetup."
+      address="chembur, mumbai-400074, India"
+      description="This is a first meetup."
+    />
+  );
+};
+
+export default MeetupDetailsPage;
